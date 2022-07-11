9823e9a6597175bb3915a6e2c51a3aafb7cc7b5f
Eythan01ch <93129649+Eythan01ch@users.noreply.github.com>
2022-07-11 10:46:55 +0300

    a

--- proxy/run.sh
+++ proxy/run.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-envsubst < /etc/nginx/default.congf.tpl > /etc/nginx/conf.d/default.congf
+envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
